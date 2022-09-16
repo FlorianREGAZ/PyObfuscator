e753f268eca0e74bf67f4e228457ecb6421c5d54
Florian <trayhd4415@gmail.com>
2022-09-16 10:09:35 +0200

    general

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# TODO
+# PyObfuscator
 
-- have all operations (+, -, *, / etc.) in opcodes
-- have all in house functions as opcodes (print, sum etc.)
+Make your code difficult to understand and read. Minimize exposure to attacks.
+
+This is an educational project to learn more about the Abstract-Syntax-Tree library of python and to explore the possibilities you have with python obfuscation.
+
+The project is still in an very early stage of development. It currently only works on very simple scripts, so use it at your own risk!
+
+## Examples
+test
+
+## Planned Features
+- Support for more complex code (classes, more operations etc.)
+- Anti debugging method
+- One line obfuscation
+- many more..
--- examples/example_custom_function.py
+++ examples/example_custom_function.py
@@ -10,16 +10,6 @@ print_sum(number1, number2)
 
 # Translates to --------------------------------------------------------------------------------------------------------
 def function1(arg1, arg2):
-    print(arg1 + arg2)
-
-CUSTOM_FUNCTIONS[0] = function1
-MEMORY[0] = 1
-MEMORY[1] = 5
-
-MEMORY[2] = execute_function(0, 0, 1)
-
-# Translates to --------------------------------------------------------------------------------------------------------
-def function1(arg1, arg2):
     print(OPCODES[0](arg1, arg2))
 
 CUSTOM_FUNCTIONS[0] = function1
@@ -30,9 +20,9 @@ MEMORY[2] = OPCODES[4](0, 0, 1)
 
 
 # Translates to --------------------------------------------------------------------------------------------------------
-
-#f[0] = print_sum
+#def function1(arg1, arg2):
+#    print(OPCODES[0](arg1, arg2))
+#f[0] = function1
 #m[0] = 1
 #m[1] = 5
-#
 #m[2] = o[4](0, 0, 1)
